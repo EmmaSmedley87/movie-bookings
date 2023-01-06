7e591784aaa1e01b565b4b8febaad1600605d2ae
EmmaSmedley87 <EmmaLondon1987@hotmail.com>
2023-01-06 18:04:50 +0000

    added an event listener to select seat and a toggle to turn selected seat blue

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
+
+container.addEventListener("click", (e) => {
+  // if statement to check if we are selecting the seat, it should not contain occupied seat
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    // toggle function to turn occupied seat to blue when clicked
+    e.target.classList.toggle("selected");
+  }
+});
