0a4303d913758896622f84d08277b4a60c819abb
EmmaSmedley87 <EmmaLondon1987@hotmail.com>
2023-01-07 22:20:37 +0000

    save selected movie index and price function created

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const container = document.querySelector(".container");
-const seat = document.querySelectorAll(".row .seat:not(.occupied");
+const seats = document.querySelectorAll(".row .seat:not(.occupied");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map(function (seat) {
+    return [...seats].indexOf(seat);
+  });
+  // save selections in local browser
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
@@ -18,6 +31,7 @@ function updateSelectedCount() {
 // movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
