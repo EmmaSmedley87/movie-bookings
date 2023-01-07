cd005c156212e33d49a73ce730e5402cc7bf1472
EmmaSmedley87 <EmmaLondon1987@hotmail.com>
2023-01-07 22:54:03 +0000

    created a function populateUI function and if statement to save the selected movie seats on browser refresh

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+// get data from local storage and populate UI
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // save selected movie index and price
@@ -28,6 +31,24 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex != null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
@@ -47,3 +68,5 @@ container.addEventListener("click", (e) => {
   // count function to update price when seat is selected
   updateSelectedCount();
 });
+
+updateSelectedCount();
